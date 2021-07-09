a10ef02df811384ce0f65963f3479dafbcacdf2d
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2021-07-09 14:51:10 +0200

    Support getting Container metadata

--- combined.go
+++ combined.go
@@ -7,10 +7,15 @@ import (
 	"os"
 )
 
+// Will retrieve the task metadata for compatibility
+func Get(ctx context.Context, client *http.Client) (interface{}, error) {
+	return GetTask(ctx, client)
+}
+
 // Will retrieve the task metadata
 // for your current Fargate environment (either V3 or V4)
 // based on the environment variables that are present
-func Get(ctx context.Context, client *http.Client) (interface{}, error) {
+func GetTask(ctx context.Context, client *http.Client) (interface{}, error) {
 	// If the ECS Metadata URI for v4 is set,
 	// use this. When running on platform version 4,
 	// v3 might also still be set, though we prioritize the newer format
@@ -24,6 +29,28 @@ func Get(ctx context.Context, client *http.Client) (interface{}, error) {
 	isV3 := os.Getenv(ecsMetadataUriEnvV3) != ""
 	if isV3 {
 		return GetTaskV3(ctx, client)
+	}
+
+	return nil, fmt.Errorf("could not resolve ECS Task metadata")
+}
+
+// Will retrieve the container metadata
+// for your current Fargate environment (either V3 or V4)
+// based on the environment variables that are present
+func GetContainer(ctx context.Context, client *http.Client) (interface{}, error) {
+	// If the ECS Metadata URI for v4 is set,
+	// use this. When running on platform version 4,
+	// v3 might also still be set, though we prioritize the newer format
+	isV4 := os.Getenv(ecsMetadataUriEnvV4) != ""
+	if isV4 {
+		return GetContainerV4(ctx, client)
+	}
+
+	// If the Metadata URI for v4 wasn't set,
+	// check for v3
+	isV3 := os.Getenv(ecsMetadataUriEnvV3) != ""
+	if isV3 {
+		return GetContainerV3(ctx, client)
 	}
 
 	return nil, fmt.Errorf("could not resolve ECS Task metadata")
--- v3.go
+++ v3.go
@@ -15,41 +15,43 @@ const (
 	ecsMetadataUriEnvV3 = "ECS_CONTAINER_METADATA_URI"
 )
 
-type TaskMetadataV3 struct {
-	Cluster       string `json:"Cluster"`
-	TaskARN       string `json:"TaskARN"`
-	Family        string `json:"Family"`
-	Revision      string `json:"Revision"`
+type ContainerMetadataV3 struct {
+	DockerID   string `json:"DockerId"`
+	Name       string `json:"Name"`
+	DockerName string `json:"DockerName"`
+	Image      string `json:"Image"`
+	ImageID    string `json:"ImageID"`
+	Labels     struct {
+		EcsCluster               string `json:"com.amazonaws.ecs.cluster"`
+		EcsContainerName         string `json:"com.amazonaws.ecs.container-name"`
+		EcsTaskArn               string `json:"com.amazonaws.ecs.task-arn"`
+		EcsTaskDefinitionFamily  string `json:"com.amazonaws.ecs.task-definition-family"`
+		EcsTaskDefinitionVersion string `json:"com.amazonaws.ecs.task-definition-version"`
+	} `json:"Labels"`
 	DesiredStatus string `json:"DesiredStatus"`
 	KnownStatus   string `json:"KnownStatus"`
-	Containers    []struct {
-		DockerID   string `json:"DockerId"`
-		Name       string `json:"Name"`
-		DockerName string `json:"DockerName"`
-		Image      string `json:"Image"`
-		ImageID    string `json:"ImageID"`
-		Labels     struct {
-			EcsCluster               string `json:"com.amazonaws.ecs.cluster"`
-			EcsContainerName         string `json:"com.amazonaws.ecs.container-name"`
-			EcsTaskArn               string `json:"com.amazonaws.ecs.task-arn"`
-			EcsTaskDefinitionFamily  string `json:"com.amazonaws.ecs.task-definition-family"`
-			EcsTaskDefinitionVersion string `json:"com.amazonaws.ecs.task-definition-version"`
-		} `json:"Labels"`
-		DesiredStatus string `json:"DesiredStatus"`
-		KnownStatus   string `json:"KnownStatus"`
-		Limits        struct {
-			CPU    int `json:"CPU"`
-			Memory int `json:"Memory"`
-		} `json:"Limits"`
-		CreatedAt time.Time `json:"CreatedAt"`
-		StartedAt time.Time `json:"StartedAt,omitempty"`
-		Type      string    `json:"Type"`
-		Networks  []struct {
-			NetworkMode   string   `json:"NetworkMode"`
-			IPv4Addresses []string `json:"IPv4Addresses"`
-		} `json:"Networks"`
-	} `json:"Containers"`
-	Limits struct {
+	Limits        struct {
+		CPU    int `json:"CPU"`
+		Memory int `json:"Memory"`
+	} `json:"Limits"`
+	CreatedAt time.Time `json:"CreatedAt"`
+	StartedAt time.Time `json:"StartedAt,omitempty"`
+	Type      string    `json:"Type"`
+	Networks  []struct {
+		NetworkMode   string   `json:"NetworkMode"`
+		IPv4Addresses []string `json:"IPv4Addresses"`
+	} `json:"Networks"`
+}
+
+type TaskMetadataV3 struct {
+	Cluster       string                `json:"Cluster"`
+	TaskARN       string                `json:"TaskARN"`
+	Family        string                `json:"Family"`
+	Revision      string                `json:"Revision"`
+	DesiredStatus string                `json:"DesiredStatus"`
+	KnownStatus   string                `json:"KnownStatus"`
+	Containers    []ContainerMetadataV3 `json:"Containers"`
+	Limits        struct {
 		CPU    float64 `json:"CPU"`
 		Memory int     `json:"Memory"`
 	} `json:"Limits"`
@@ -73,4 +75,22 @@ func GetTaskV3(ctx context.Context, client *http.Client) (*TaskMetadataV3, error
 	}
 
 	return taskMetadata, nil
+}
+
+// Retrieve ECS Container Metadata in V3 format
+func GetContainerV3(ctx context.Context, client *http.Client) (*ContainerMetadataV3, error) {
+	metadataUrl := os.Getenv(ecsMetadataUriEnvV3)
+	if metadataUrl == "" {
+		return nil, fmt.Errorf("missing metadata uri in environment (%s)", ecsMetadataUriEnvV3)
+	}
+
+	contaienrMetadata := &ContainerMetadataV3{}
+	body, err := fetch(ctx, client, fmt.Sprintf("%s", metadataUrl))
+
+	err = json.Unmarshal(body, &contaienrMetadata)
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal into container metadata (v3): %w", err)
+	}
+
+	return contaienrMetadata, nil
 }
