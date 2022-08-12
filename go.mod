b6b31c6eeeaf165db79be27748909f3b48c19f71
Michele Mancioppi <michelem@lumigo.io>
2022-08-12 17:08:32 +0200

    Change the type of AwsLogsCreateGroup to string (#7)
    * Change the type of AwsLogsCreateGroup to string
    
    * Add basic test pipeline

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/brunoscheufler/aws-ecs-metadata-go
 
 go 1.14
+
+require github.com/stretchr/testify v1.8.0 // indirect
---
+++ v4_test.go
@@ -0,0 +1,37 @@
+package metadata
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseContainerMetadataV4(t *testing.T) {
+	content, err := os.ReadFile("testdata/metadatav4-response-container.json")
+
+	assert.NoError(t, err, "FAILED")
+
+	containerMetadata := &ContainerMetadataV4{}
+	json.Unmarshal(content, containerMetadata)
+
+	assert.Equal(t, "/ecs/metadata", containerMetadata.LogOptions.AwsLogsGroup)
+	assert.Equal(t, "ecs/curl/8f03e41243824aea923aca126495f665", containerMetadata.LogOptions.AwsLogsStream)
+	assert.Equal(t, "us-west-2", containerMetadata.LogOptions.AwsRegion)
+	assert.Equal(t, "true", containerMetadata.LogOptions.AwsLogsCreateGroup)
+}
+
+func TestParseTaskMetadataV4(t *testing.T) {
+	content, err := os.ReadFile("testdata/metadatav4-response-task.json")
+
+	assert.NoError(t, err, "FAILED")
+
+	taskMetadata := &TaskMetadataV4{}
+	json.Unmarshal(content, taskMetadata)
+
+	assert.Equal(t, "/ecs/metadata", taskMetadata.Containers[1].LogOptions.AwsLogsGroup)
+	assert.Equal(t, "ecs/curl/158d1c8083dd49d6b527399fd6414f5c", taskMetadata.Containers[1].LogOptions.AwsLogsStream)
+	assert.Equal(t, "us-west-2", taskMetadata.Containers[1].LogOptions.AwsRegion)
+	assert.Equal(t, "true", taskMetadata.Containers[1].LogOptions.AwsLogsCreateGroup)
+}
